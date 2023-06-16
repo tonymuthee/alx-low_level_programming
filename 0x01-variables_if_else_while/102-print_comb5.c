705956d88d3dc9c9d89eee329ed2c39f224d50a9
tony <tonionga@gmail.com>
2023-06-16 07:42:27 -0700

    comb try

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,7 +6,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+/**int main(void)
 {
 	int a;
 	int b;
@@ -43,6 +43,29 @@ int main(void)
 	}
 	}
 	}
+	}
+	putchar('\n');
+	return (0);
+}*/
+int main(void)
+{
+	int p, q;
+
+	for (p = 0; p <= 98; p++)
+	{
+		for (q = p + 1; q <= 99; q++)
+		{
+			putchar((p / 10) + '0');
+			putchar((p % 10) + '0');
+			putchar(' ');
+			putchar((q / 10) + '0');
+			putchar((q % 10) + '0');
+
+			if (p == 98 && q == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
