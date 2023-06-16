bd528aac21c27061a35f452553277f9f51b3289d
tony <tonionga@gmail.com>
2023-06-16 07:44:34 -0700

    comb fixed

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,47 +6,6 @@
  *
  * Return: Always 0 (Success)
  */
-/**int main(void)
-{
-	int a;
-	int b;
-	int c;
-	int d;
-
-	for (a = 0; a <= 9; a++)
-	{
-	for (b = 0; b <= 9; b++)
-	{
-	for (c = 0; c <= 9; c++)
-	{
-	for (d = 0; d <= 9; d++)
-	{
-		if (c < a || c < b || d < b)
-	{
-		continue;
-	}
-	else if (a == c && b == d)
-	{
-		continue;
-	}
-	putchar(a + '0');
-	putchar(b + '0');
-	putchar(' ');
-	putchar(c + '0');
-	putchar(d + '0');
-	if (a == 9 && b == 8 && c == 9 && d == 9)
-	{
-		break;
-	}
-	putchar(',');
-	putchar(' ');
-	}
-	}
-	}
-	}
-	putchar('\n');
-	return (0);
-}*/
 int main(void)
 {
 	int p, q;
